7d0d218d055ac0b03b34a88168a9a07dee7ac277
Spencer Dent <mr.spencerdent@gmail.com>
2020-06-12 09:31:16 -0400

    Fix: include all attribute data on sheet
    * pull rest of attributes and skills data from sheet
    * better modularize attributes article
    * improve display for visibility
    * fix character name to display on one line

--- src/App.js
+++ src/App.js
@@ -7,16 +7,28 @@ import Section from './elements/Section'
 
 import jamesWest from './data/james-west.json'
 
-const App = () => <>
+const {
+  username,
+  subject_profile,
+  base_values,
+  attributes,
+  medical,
+  inventory_signOuts,
+  subject_bio,
+  brief,
+  career_history
+} = jamesWest
+
+const App = () => <main>
   <h1>Twilight: 2000 &ndash; Character Record Sheet</h1>
-  <Section name="subject_profile" data={jamesWest.subject_profile} />
-  <Section name="base_values" data={jamesWest.base_values} />
-  <Attributes attributes={jamesWest.attributes_and_skills} />
-  <Section name="medical" data={jamesWest.medical} />
-  <Section name="inventory_sign-outs" data={jamesWest["inventory_sign-outs"]} />
-  <Section name="subject_bio" data={jamesWest.subject_bio} />
-  <Section name="brief" data={jamesWest.brief} />
-  <Section name="career_history" data={jamesWest.career_history} />
-</>
+  <Section name="subject_profile" data={subject_profile} />
+  <Section name="base_values" data={base_values} />
+  <Attributes user={username} data={attributes} />
+  <Section name="medical" data={medical} />
+  <Section name="inventory_sign-outs" data={inventory_signOuts} />
+  <Section name="subject_bio" data={subject_bio} />
+  <Section name="brief" data={brief} />
+  <Section name="career_history" data={career_history} />
+</main>
 
 export default App
--- src/elements/Attributes.js
+++ src/elements/Attributes.js
@@ -1,40 +1,59 @@
 import React from 'react'
 
-const Attribute = ({ data }) => {
+const Skill = ({ stat, data, key }) => {
+  const { name, level } = data
+  const hasLevel = level !== null && level !== undefined && !isNaN(level)
+
+  return <tr key={key}>
+    <td>{name}</td>
+    <td>{hasLevel ? level : '--'}</td>
+    <td>{hasLevel ? stat + level : <span className="danger">UNSKILLED</span>}</td>
+  </tr>
+}
+
+const Attribute = ({ data, key }) => {
   const { name, stat, skills } = data
 
-  return <section className="attribute">
+  return <section id={name + '_attribute'} key={key} className="attribute">
     <h3>{name}: {stat}</h3>
     <table>
       <thead>
         <tr>
-          <th></th>
+          <th>Skill</th>
           <th>Level</th>
           <th>Asset</th>
         </tr>
       </thead>
       <tbody>
-        {skills.map(({ name, level }, i) => {
-          const hasLevel = level !== null && level !== undefined && !isNaN(level)
+        {skills.map((skill, i) => {
+          const subSkills = skill['sub-skills']
 
-          return (
-            <tr key={i}>
-              <td>{name}</td>
-              <td>{hasLevel ? level : '--'}</td>
-              <td>{hasLevel ? stat + level : <span className="danger">UNSKILLED</span>}</td>
-            </tr>
-          )
+          return subSkills
+            ? <><Skill stat={stat} data={skill} key={i} />{subSkills.map((sub, i) => <Skill stat={stat} data={{ ...sub, name: ' - ' + sub.name }} key={i} />)}</>
+            : <Skill stat={stat} data={skill} key={i} />
         })}
       </tbody>
     </table>
   </section>
 }
 
-const Attributes = ({ attributes }) => <section>
-  <h2>Attributes and Skills</h2>
-  <div id="attributes">
-    {attributes.map((attr, i) => <Attribute data={attr} key={i} />)}
-  </div>
-</section>
+const Attributes = ({ user, data }) => <>
+  <h2>ATTRIBUTES</h2>
+  <table>
+    <tbody>
+      <tr>
+        <td>PROFILE:</td>
+        <td>{user}</td>
+      </tr>
+      <tr>
+        <td>TIME/DATE:</td>
+        <td>{(new Date()).toLocaleString()}</td>
+      </tr>
+    </tbody>
+  </table>
+  <article id="attributes">
+    {data.map((attribute, i) => <Attribute data={attribute} key={i} />)}
+  </article>
+</>
 
 export default Attributes
